df23b6058098c481d4f7bd14b52b21073b1b3c3a
msakac <msakac@foi.hr>
2022-12-16 10:56:32 +0100

    promjene

--- index.js
+++ index.js
@@ -220,6 +220,56 @@ app.get('/arena', (req, res) => {
     });
 });
 
+//Supersport scraper
+app.get('/supersport', (req, res) => {
+    let parovi = [];
+    let brojUtakmica = 0;
+
+    fs.readFile('supersport.txt', 'utf8', (err, fileData) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        const $ = cheerio.load(fileData);
+        $('tr', fileData).each(function () {
+            const trPonuda = $(this);
+            let domacin, gost;
+            let koef = {};
+            brojUtakmica++;
+            const spanPonudaNaziv = trPonuda.find('.ponuda-naziv');
+            const timovi = [];
+            $('span', spanPonudaNaziv).each(function (){
+                span = $(this);
+                if(!span.hasClass('separator') && !span.hasClass('text-pored')){
+                    timovi.push(span.text());
+                }
+            });
+            domacin = timovi[0];
+            gost = timovi[1];
+            let koeficijenti = [];
+            $('.tecaj-text', trPonuda).each(function (){
+                const divTecajText = $(this);
+                koeficijenti.push(divTecajText.text());
+            });
+            if(koeficijenti.length > 0){
+                koef= {
+                    domacin: koeficijenti[0],
+                    nerjeseno: koeficijenti[1],
+                    gost: koeficijenti[2],
+                    domacinNerjeseno: koeficijenti[3],
+                    gostNerjeseno: koeficijenti[4],
+                    domacinGost: koeficijenti[5],
+                }
+            }
+            if(domacin != null && gost != null){
+                parovi.push({ domacin, gost, koef });
+            }
+        });
+        res.json(parovi);
+    });
+});
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
 
 
---
+++ pomoc.js
@@ -0,0 +1,48 @@
+app.get('/germania', (req, res) => {
+    let parovi = [];
+    let brojUtakmica = 0;
+    //citam html iz txt filea
+    fs.readFile('germania.txt', 'utf8', (err, fileData) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        //parsiram html
+        const $ = cheerio.load(fileData);
+        //trazim sve divove sa klasom match
+        $('div .match', fileData).each(function () {
+            const divMatch = $(this);
+            let domacin, gost;
+            let koef = {};
+            //trazim sve divove sa klasom sp-mark koja oznaca da se radi o specijalnoj ponudi
+            const specijalnaPonuda = divMatch.find('.sp-mark').text();
+            //ako nema specijalne ponude mogu obraditi podatke
+            if (specijalnaPonuda == '') {
+                let podaciUtakmice = [];
+                //iteriram se kroz sve spanove i nutra stavljam sve tekstove koji su u njima
+                $('span', divMatch).each(function () {
+                    span = $(this);
+                    podaciUtakmice.push(span.text());
+                });
+                //postavljanje potrebnih podataka
+                domacin = podaciUtakmice[2];
+                gost = podaciUtakmice[3];
+                koef= {
+                    domacin: podaciUtakmice[4],
+                    nerjeseno: podaciUtakmice[6],
+                    gost: podaciUtakmice[8],
+                    domacinNerjeseno: podaciUtakmice[10],
+                    gostNerjeseno: podaciUtakmice[12],
+                    domacinGost: podaciUtakmice[14],
+                }
+                //console.log(podaciUtakmice);
+            }
+            if(domacin != null && gost != null && koef != null){
+                brojUtakmica++;
+                parovi.push({ domacin, gost, koef });
+            }
+        })
+        console.log('Broj utakmica Germania: ' + brojUtakmica);
+        res.json(parovi);
+    });
+});
